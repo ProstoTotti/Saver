c73d91c4d0ec395d442001313db900737dc1dd4d
ProstoTotti <prostototti@gmail.com>
2017-09-29 17:48:02 +0300

    swift4

--- Saver/AppDelegate.swift
+++ Saver/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UINavigationBar.appearance().barTintColor = .black
         UINavigationBar.appearance().tintColor = .white
-        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
         UIApplication.shared.statusBarStyle = .lightContent
         
         
